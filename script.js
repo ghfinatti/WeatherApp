ba5b314a3034c1a2d56513c5c2b9bf7416984254
ghfinatti <ghfinatti3@gmail.com>
2022-01-04 21:48:09 -0300

    header done

---
+++ script.js
@@ -0,0 +1,8 @@
+function getData(place) {
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&appid=b4cb66d12dd3678944954c55ddafe2d8`, {mode: 'cors'})
+    .then(response => response.json())
+    .then(function(data){
+        console.log(data)
+    })
+}
+
