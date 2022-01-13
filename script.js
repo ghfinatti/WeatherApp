a969122db7525e8100ce29cb10811d932b1fac98
ghfinatti <ghfinatti3@gmail.com>
2022-01-13 19:24:56 -0300

    deploy

--- script.js
+++ script.js
@@ -5,28 +5,37 @@ const windData = document.querySelector('.winddata');
 const main = document.querySelector('main');
 const searchBtn = document.querySelector('.arrowicon');
 const searchBar = document.querySelector('.searchbar');
+const unitsBtn = document.querySelector('.unitsbtn');
 
-function getData(place, units) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&units=${units}&appid=b4cb66d12dd3678944954c55ddafe2d8`, {mode: 'cors'})
+
+async function getData(place, units) {
+    await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${place}&units=${units}&appid=b4cb66d12dd3678944954c55ddafe2d8`, {mode: 'cors'})
     .then(response => response.json())
     .then(function(data){
         if(data.cod == "404"){
             alert("City name not found.")
         }else{
-            changeDom(data.name, data.main.temp, data.main.humidity, data.wind.speed, data.weather[0].main)
+            changeDom(data.name, data.main.temp, data.main.humidity, data.wind.speed, data.weather[0].main,unitsBtn.dataset.unit)
         }
-        console.log(data)
     })
     .catch(function(err){
         console.log(err)
     })
 }
 
-function changeDom(name, temp, humidity, wind, weather){
-    cityName.textContent = `${name}`;
-    tempData.textContent = `${Math.round(temp)} ºC`;
-    humidityData.textContent = `${humidity}%`;
-    windData.textContent = `${wind} km/h`;
+function changeDom(name, temp, humidity, wind, weather, unit){
+    if (unit == "metric"){
+        cityName.textContent = `${name}`;
+        tempData.textContent = `${Math.round(temp)}ºC`;
+        humidityData.textContent = `${humidity}%`;
+        windData.textContent = `${wind} m/s`;
+    };
+    if (unit == "imperial"){
+        cityName.textContent = `${name}`;
+        tempData.textContent = `${Math.round(temp)}ºF`;
+        humidityData.textContent = `${humidity}%`;
+        windData.textContent = `${wind} mph`;
+    };
 
     if (weather == "Rain"){
         main.style.backgroundImage = "url('images/rain.jpg')";
@@ -43,15 +52,32 @@ function changeDom(name, temp, humidity, wind, weather){
 }
 
 function searchBtnClick(){
-    getData(searchBar.value,"metric");
+    if (searchBar.value == ""){
+        alert("Please choose a city name.");
+    }else{
+        const currentUnitType = unitsBtn.dataset.unit
+        getData(searchBar.value, currentUnitType);
+    };
 }
 
-getData("London","metric")
+function changeUnit() {
+    if (unitsBtn.dataset.unit == "metric"){
+        unitsBtn.textContent = "Units: Imperial";
+        unitsBtn.dataset.unit = "imperial";
+        getData(cityName.textContent,"imperial");
+    }else{
+        unitsBtn.textContent = "Units: Metric";
+        unitsBtn.dataset.unit = "metric";
+        getData(cityName.textContent,"metric");
+    }
+}
 
 searchBtn.addEventListener('click', searchBtnClick)
-
 searchBar.addEventListener("keyup", function(event) {
     if (event.keyCode === 13) {
       searchBtn.click();
     }
 });
+unitsBtn.addEventListener('click', changeUnit);
+
+getData("London", "metric")
